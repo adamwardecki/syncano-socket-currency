14a285bb22fe7c4dbcfc6f37a30f2ea4adb483b3
Adam Wardecki <adam.wardecki@syncano.com>
2017-02-27 17:55:32 +0100

    syncano-socket-currency

---
+++ scripts/convert.js
@@ -0,0 +1,53 @@
+var axios = require('axios');
+var fx = require('money');
+var connect = require('syncano-server').default;
+
+
+const { data, event } = connect()
+
+function response(code, content) {
+  setResponse(new HttpResponse(code, JSON.stringify(content), 'application/json'));
+  return process.exit();
+}
+
+function convert(from, to, amount) {
+  // Calling fixer API. Documentation can be found here: http://fixer.io/
+  return axios.get('http://api.fixer.io/latest')
+    .then((response) => {
+  // Money.js https://openexchangerates.github.io/money.js/
+      fx.rates = response.data.rates;
+      fx.base = response.data.base;
+
+      return fx(amount).from(from).to(to);
+    })
+    .catch((error) => {
+      response(400, JSON.stringify(error));
+    });
+}
+
+const from = ARGS.from;
+const to = ARGS.to;
+const amount = ARGS.amount;
+
+const fromError = `From '${from}' value is not valid. It should be a string`
+const toError = `To '${to}' value is not valid. It should be a string`
+const amountError = `Amount '${amount}' value is not valid. It should be a number`
+
+if (typeof from !== 'string') response(400, {"error": fromError});
+if (typeof to !== 'string') response(400, {"error": toError});
+if (typeof amount !== 'number') response(400, {"error": amountError});
+
+
+convert(from, to, amount)
+  .then(function(rate) {
+    // Line below uses Syncano data storage features
+    // Log the conversion info in in the conversion_logs class for future reference
+    data.conversion_logs.create({from, to, amount, rate});
+
+    const error = 'Please provide proper currency value (i.e. "USD")'
+    if (typeof rate !== 'number') response(400, {error});
+
+    response(200, {rate})
+
+})
+  .catch((error) => response(400, error));
